8aabc60ae1d7a1e7481e644d8a1c1a12758c9eb3
veemyrtus <veenaraofr@gmail.com>
2025-12-01 16:11:15 +0100

    cd test

--- tests/test_math.py
+++ tests/test_math.py
@@ -2,10 +2,11 @@ import pytest
 import sys
 import os
 
-# Add parent directory to path to import math module
+# Add parent directory to path to import our custom math module
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from math import add, subtract
+# Import from our custom math module (not the built-in math module)
+import math as our_math
 
 
 class TestMath:
@@ -13,31 +14,31 @@ class TestMath:
 
     def test_add(self):
         """Test addition"""
-        assert add(2, 3) == 5
-        assert add(-1, 1) == 0
-        assert add(0, 0) == 0
+        assert our_math.add(2, 3) == 5
+        assert our_math.add(-1, 1) == 0
+        assert our_math.add(0, 0) == 0
 
     def test_subtract(self):
         """Test subtraction"""
-        assert subtract(5, 3) == 2
-        assert subtract(0, 5) == -5
-        assert subtract(-1, -1) == 0
+        assert our_math.subtract(5, 3) == 2
+        assert our_math.subtract(0, 5) == -5
+        assert our_math.subtract(-1, -1) == 0
 
     def test_add_floats(self):
         """Test addition with floats"""
-        assert add(2.5, 3.5) == 6.0
-        assert add(0.1, 0.2) > 0.2  # Floating point check
+        assert our_math.add(2.5, 3.5) == 6.0
+        assert our_math.add(0.1, 0.2) > 0.2  # Floating point check
 
     def test_subtract_floats(self):
         """Test subtraction with floats"""
-        assert subtract(5.5, 2.5) == 3.0
+        assert our_math.subtract(5.5, 2.5) == 3.0
 
 
 def test_add_simple():
     """Simple standalone test"""
-    assert add(1, 1) == 2
+    assert our_math.add(1, 1) == 2
 
 
 def test_subtract_simple():
     """Simple standalone test"""
-    assert subtract(10, 5) == 5
+    assert our_math.subtract(10, 5) == 5
